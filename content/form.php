2c8c75574b0d63d205bb672c25af1bd10f4b221c
[tama] <[tamasupaka@gmail.com]>
2021-02-18 14:02:42 +0900

    アイデア・コメント投稿時のユーザー情報割り当て処理追加 セッションが行われていない場合登校できなくなります．

--- content/form.php
+++ content/form.php
@@ -28,10 +28,16 @@ require '../inc/header.php';
         });
     </script>
     <h2 id="new">アイデア投稿フォーム</h2>
-    <div id="form">
-        <textarea id="idea_form" cols="30" rows="10"></textarea>
-        <button id="sendBtn" class="btn btn--orange btn--radius">投稿する</button>
-    </div>
+    <?php
+    if (isset($_SESSION['user_id'])) {
+        echo '<div id="form">';
+        echo '<textarea id="idea_form" cols="30" rows="10"></textarea>';
+        echo '<button id="sendBtn" class="btn btn--orange btn--radius">投稿する</button>';
+        echo '</div>';
+    }else{
+        echo '<div id="form"><p>投稿にはログインが必要です</p></div>';
+    }
+    ?>
     <div id="watch_idea">
         <a href="idea_list.php" class="btn btn--orange btn--radius">アイデアを見に行く</a>
     </div>
